ce0f903987fa47cee20e173c8176b32c6f4771f0
Kishechnikov Vlad <v.koshkin99@gmail.com>
2018-05-29 12:01:58 +0300

    7

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ main() {
     int a,b;
     cout << "Enter A and B : ";
  cin>>a>>b;
-    cout << "A + B = " << a + b << '\n';
+    cout << " A + B = " << a + b << '\n';
+    cout << " A * 2 = " << a * 2 << '\n';
 }
